6a6a24d4da62432868afb4816926f6ba9360c2df
Mario.v.B <67900070+Mvb-DL@users.noreply.github.com>
2024-06-17 12:02:22 +0200

    set up media pop up

--- src/screens/home/HomeScreen.jsx
+++ src/screens/home/HomeScreen.jsx
@@ -11,18 +11,44 @@ import Projects from "../../components/projects/Projects"
 import LifeLine from "../../components/lifeLine/LifeLine"
 import {Helmet} from "react-helmet";
 import { GlobalStateProvider } from "../../components/changeLang/ChangeLang"
+import MediaPopUp from "../../components/MediaPopUp/mediaPopUp";
+import { useMediaQuery } from 'react-responsive';
+import React, { useState, useEffect } from 'react';
+
 
 const HomeScreen = () => {
+
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  const isDesktop = useMediaQuery({ minWidth: 768 });
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    if (isMobile) {
+      setIsModalOpen(true);
+    } else if (isDesktop) {
+      setIsModalOpen(false);
+    }
+  }, [isMobile, isDesktop]);
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
   return (
 
     <div className="page-container">
 
-    <GlobalStateProvider>
-
-        <BackgroundEffect />
+      <MediaPopUp isOpen={isModalOpen} onClose={closeModal}>
+        <h2>Bessere Experience auf dem Desktop</h2>
+        <p>Für eine bessere Website-Experience bitte auf einem Desktop öffnen!</p>
+        <button style={{color: "#fff", background: "#6225E6", marginTop: "3%", padding: "3px", borderRadius: "5px"}} onClick={closeModal}>Close</button>
+      </MediaPopUp>
 
         <CookieConsent></CookieConsent>
+
+        <GlobalStateProvider>
+
+        <BackgroundEffect />
 
         <LangOptions></LangOptions>
         
