6ed818bb9dafcbc01ebb00bff70f2525527f5311
Mario.v.B <67900070+Mvb-DL@users.noreply.github.com>
2024-07-02 14:32:32 +0200

    back

--- vite.config.js
+++ vite.config.js
@@ -2,40 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import ViteGltfPlugin from 'vite-plugin-gltf';
 
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
+  plugins: [react(),
     ViteGltfPlugin({
-      include: [/\.glb$/],
-    })
-  ],
-  base: "/ApplicationWebsite/",
-  build: {
-    rollupOptions: {
-      output: {
-        // Define manual chunks for code-splitting
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react') || id.includes('react-dom')) {
-              return 'react-vendor';
-            }
-            if (id.includes('three')) {
-              return 'three-vendor';
-            }
-            // Add other libraries or chunks as needed
-            return 'vendor';
-          }
-        },
-      },
-    },
-    // Enable CSS code splitting
-    cssCodeSplit: true,
-    // Enable Brotli compression
-    brotliSize: true,
-    // Adjust the chunk size warning limit if necessary
-    chunkSizeWarningLimit: 1000, // Adjust the limit as needed
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'three'],
-  },
+      include: [/\.glb$/],  
+    })],
+    base: "/ApplicationWebsite/"
 });
+
+
+
