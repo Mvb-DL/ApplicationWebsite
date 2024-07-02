2bba57644ffce897a9c598af171f47820e4320cb
Mario.v.B <67900070+Mvb-DL@users.noreply.github.com>
2024-07-02 19:54:12 +0200

    set up config

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,26 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import ViteGltfPlugin from 'vite-plugin-gltf';
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     ViteGltfPlugin({
-      include: [/\.glb$/],  
-    })],
-    base: "/ApplicationWebsite/"
+      include: [/\.glb$/],
+    }),
+  ],
+  base: "/ApplicationWebsite/",
+  build: {
+   
+    cssCodeSplit: true,
+    brotliSize: true,
+    chunkSizeWarningLimit: 500,
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'three'],
+  },
 });
-
-
-
