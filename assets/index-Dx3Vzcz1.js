23e0944c7b3aec8fbe090f4a8c9d2266e6be46e0
Mario.v.B <67900070+Mvb-DL@users.noreply.github.com>
2024-07-07 17:59:40 +0200

    Updates

--- assets/index-Dx3Vzcz1.js
+++
@@ -1,2 +0,0 @@
-const __vite__fileDeps=["assets/SizeUpdater-ICyaRlFO.js","assets/NumberUtils-fSLHzFkB.js","assets/TypeUtils-BOyOcFPO.js","assets/Utils-B52he1Wb.js"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
-import{_ as r}from"./index-B6xMlBZX.js";async function o(t,a=!0){await t.addParticleUpdater("size",async()=>{const{SizeUpdater:e}=await r(()=>import("./SizeUpdater-ICyaRlFO.js"),__vite__mapDeps([0,1,2,3]));return new e},a)}export{o as loadSizeUpdater};
