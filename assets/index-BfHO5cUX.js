65a29e4ff688e0f71c2edf2505dd63b7794de75e
Mario.v.B <67900070+Mvb-DL@users.noreply.github.com>
2024-08-12 10:03:11 +0200

    Updates

--- assets/index-BfHO5cUX.js
+++
@@ -1,2 +0,0 @@
-const __vite__fileDeps=["assets/OpacityUpdater-CvItlHeQ.js","assets/Utils-B52he1Wb.js","assets/NumberUtils-fSLHzFkB.js","assets/TypeUtils-BOyOcFPO.js"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
-import{_ as i}from"./index-Dg58i7Is.js";async function p(t,a=!0){await t.addParticleUpdater("opacity",async r=>{const{OpacityUpdater:e}=await i(()=>import("./OpacityUpdater-CvItlHeQ.js"),__vite__mapDeps([0,1,2,3]));return new e(r)},a)}export{p as loadOpacityUpdater};
---
+++ assets/plugin-BHj3OUFQ.js
@@ -0,0 +1,2 @@
+const __vite__fileDeps=["assets/LinksPlugin-BQwXzaG9.js","assets/index-tA82QZv-.js","assets/index-BbVlUSMP.css"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
+import{_ as o}from"./index-tA82QZv-.js";async function r(i,n=!0){const{LinksPlugin:t}=await o(()=>import("./LinksPlugin-BQwXzaG9.js"),__vite__mapDeps([0,1,2])),a=new t;await i.addPlugin(a,n)}export{r as loadLinksPlugin};
