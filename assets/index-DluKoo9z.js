66e0db508d05feb95fd7270e924fa0763177f811
Mario.v.B <67900070+Mvb-DL@users.noreply.github.com>
2024-08-12 09:31:31 +0200

    Updates

--- assets/index-DluKoo9z.js
+++
@@ -1,2 +0,0 @@
-const __vite__fileDeps=["assets/CircleDrawer-BpTUjIu7.js","assets/TypeUtils-BOyOcFPO.js","assets/NumberUtils-fSLHzFkB.js"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
-import{_ as i}from"./index-BtcOUWqf.js";async function o(a,e=!0){const{CircleDrawer:r}=await i(()=>import("./CircleDrawer-BpTUjIu7.js"),__vite__mapDeps([0,1,2]));await a.addShape("circle",new r,e)}export{o as loadCircleShape};
---
+++ assets/plugin-Dz9cvlrc.js
@@ -0,0 +1,2 @@
+const __vite__fileDeps=["assets/LinksPlugin-ufnNbx8m.js","assets/index-Dg58i7Is.js","assets/index-BbVlUSMP.css"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
+import{_ as o}from"./index-Dg58i7Is.js";async function r(i,n=!0){const{LinksPlugin:t}=await o(()=>import("./LinksPlugin-ufnNbx8m.js"),__vite__mapDeps([0,1,2])),a=new t;await i.addPlugin(a,n)}export{r as loadLinksPlugin};
