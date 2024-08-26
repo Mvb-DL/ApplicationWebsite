51a33ea3342e43976df4fe8e1411fd2775e79aa2
Mario.v.B <67900070+Mvb-DL@users.noreply.github.com>
2024-08-26 12:05:04 +0200

    Updates

---
+++ assets/index-fN9h-3Nf.js
@@ -0,0 +1,2 @@
+const __vite__fileDeps=["assets/StarDrawer-DiWVFyta.js","assets/NumberUtils-fSLHzFkB.js","assets/TypeUtils-BOyOcFPO.js"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
+import{_ as e}from"./index-D75a7VYo.js";async function _(a,t=!0){const{StarDrawer:r}=await e(()=>import("./StarDrawer-DiWVFyta.js"),__vite__mapDeps([0,1,2]));await a.addShape("star",new r,t)}export{_ as loadStarShape};
